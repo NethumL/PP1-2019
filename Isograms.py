e4e95809b08424aee11a0c324b573b71208c94c5
NethumL <61122830+NethumL@users.noreply.github.com>
2020-03-12 23:52:42 +0530

    P7 and Isograms

---
+++ Isograms.py
@@ -0,0 +1,19 @@
+"""Isograms question"""
+
+try:
+    text = input()
+
+    valid = False  #Whether the text is an isogram
+    for char in text:
+        if char.isalpha():  #Checks if the current character is a letter
+            if text.count(char)>1:  #Checks if it has repeated
+                break
+    else:
+        valid = True
+
+    if valid:
+        print("This is an isogram")
+    else:
+        print("This is not an isogram")
+except:
+    print("Invalid Input")
---
+++ P7.py
@@ -0,0 +1,28 @@
+"""Burrows-Wheeler Transformation"""
+
+try:
+    #Getting the inputs
+    text = input()
+
+    #Checking whether only A, C, G, and T are there in the text
+    for char in text:
+        if char not in ["A", "C", "G", "T"]:
+            raise Exception
+
+    #Adding the $ sign to the end
+    text = text + "$"
+
+    #Getting the permutations
+    perm = [text]  #Initializes the list of permutations with the original
+    for i in range(len(text)-1):
+        perm.append(perm[-1][-1] + perm[-1][:-1])  #Attaching the last character in the previous permutation to the beginning
+
+    #Sorting the permutations
+    perm.sort()
+
+    #Printing out the BWT
+    for i in perm:
+        print(i[-1], end = "")
+    print()  #Moves the cursor to the next line(Not essential)
+except Exception:
+    print("Input must contain only A, C, G, and T")
